bd64673a3670ec86c597e8ffd1b5cfe097eb6237
joao-erik2077 <joaoerik2074@gmail.com>
2025-09-24 19:07:13 -0300

    feat: fechar acessibilidade ao apertar esc

--- src/js/accessibility_toggle.js
+++ src/js/accessibility_toggle.js
@@ -2,5 +2,7 @@ const accessibilityOptions = document.getElementById("accessibility-options");
 const accessibilityButton = document.getElementById("accessibility-button");
 
 accessibilityButton.addEventListener("click", () => toggleAccessibility());
+document.addEventListener("keydown", (e) => e.code === "Escape" && closeAccessibilityOptions());
 
 const toggleAccessibility = () => accessibilityOptions.classList.toggle("active");
+const closeAccessibilityOptions = () => accessibilityOptions.classList.remove("active");
